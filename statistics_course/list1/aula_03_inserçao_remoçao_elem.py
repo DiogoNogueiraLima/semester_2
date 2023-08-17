f73961b4a00a931c21477e8462f9640c6f225cf5
DiogoNogueiraLima <115439066+DiogoNogueiraLima@users.noreply.github.com>
2023-08-17 01:19:42 -0300

    Add files via upload

---
+++ statistics_course/list1/aula_03_inserçao_remoçao_elem.py
@@ -0,0 +1,3 @@
+'''os metodos INSERT e REMOVE têm complexidade O(N) no PIOR caso 
+(que seria o insert no começo da lista, porque teria que mover todos elementos para frente ou para tras)
+Já no melhor caso que tem complexidade O(1) é inserido no final da lista'''
